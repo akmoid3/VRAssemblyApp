c2f5367ab5a4b9cb063723872ed4e4f4ac24e379
akmoid3 <akmoid3e@gmail.com>
2024-08-15 21:29:18 +0200

    quit button

---
+++ Assets/ApplicationQuitManager.cs
@@ -0,0 +1,17 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ApplicationQuitManager : MonoBehaviour
+{
+    public void QuitApplication()
+    {
+#if UNITY_EDITOR
+        // Ferma la modalità di gioco in editor
+        UnityEditor.EditorApplication.isPlaying = false;
+#else
+        // Chiudi l'applicazione quando è eseguita in build
+        Application.Quit();
+#endif
+    }
+}
